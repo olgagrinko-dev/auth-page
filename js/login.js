345f563ea2d103a8cdcf234369cb902ac15ce1d5
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-03-05 13:28:08 +0300

    project

--- js/login.js
+++ js/login.js
@@ -5,30 +5,35 @@
 // то вывести alert “Вы успешно зарегистрированы / авторизованы в системе”, в зависимости 
 // от страницы. Если проверки не прошли, то вывсти alert с соответствующим сообщением об ошибке
 
-const btn = document.querySelector(`.login-btn`);
-const inp1 = document.querySelector(`.input-1`);
-const inp2 = document.querySelector(`.input-2`);
+class Test {
+    constructor() {
+        this.isValid();
+    }
 
-function isValid(mail, pwd) {
-    if (!/^[a-z0-9_\.-]+@[a-z]+\.[a-z]{2,3}$/gm.test(mail) && !/^\+[0-9]+ \([0-9]+\)+ [0-9]+\-[0-9]+\-[0-9]+$/gm.test(mail)) throw new Error(`Некорректный ввод логина!`)
-    if (!/^[A-Za-z0-9!@#$%^&*()_+-=]{8,}$/gm.test(pwd)) throw new Error(`Некорректный ввод пароля!`)
+    isValid(mail, pwd) {
+        if (!/^[a-z0-9_\.-]+@[a-z]+\.[a-z]{2,3}$/gm.test(mail) && !/^\+[0-9]+ \([0-9]+\)+ [0-9]+\-[0-9]+\-[0-9]+$/gm.test(mail)) throw new Error(`Некорректный ввод логина!`)
+        if (!/^[A-Za-z0-9!@#$%^&*()_+-=]{8,}$/gm.test(pwd)) throw new Error(`Некорректный ввод пароля!`)
+    }
+
+    loginPage() {
+        const btn = document.querySelector(`.login-btn`);
+        const inp1 = document.querySelector(`.input-1`);
+        const inp2 = document.querySelector(`.input-2`);
+        btn.addEventListener(`click`, function () {
+            try {
+                let mail = inp1.value;
+                let pwd = inp2.value;
+
+                this.isValid(mail, pwd)
+
+                alert(`Добро пожаловать!`);
+                inp2.value = ``;
+
+            } catch (error) {
+                alert(error.message);
+            }
+        })
+    }
 }
 
-btn.addEventListener(`click`, function () {
-    try {        
-        let mail = inp1.value;
-        let pwd = inp2.value;
-
-        isValid(mail, pwd)
-
-        alert(`Добро пожаловать!`);
-        inp2.value = ``;
-        
-    } catch (error) {
-        alert(error.message);
-    }
-})
-
-
-
-
+const test = new Test();
--- js/sign.js
+++ js/sign.js
@@ -5,31 +5,39 @@
 // то вывести alert “Вы успешно зарегистрированы / авторизованы в системе”, в зависимости 
 // от страницы. Если проверки не прошли, то вывсти alert с соответствующим сообщением об ошибке
 
-const btn = document.querySelector(`.sign-btn`);
-const inp1 = document.querySelector(`.input-1`);
-const inp2 = document.querySelector(`.input-2`);
-const inp3 = document.querySelector(`.input-3`);
+class Test {
+    constructor() {
+        this.isValid();
+    }
 
-function isValid(mail, pwd, conPwd) {
-    if (!/^[a-z0-9_\.-]+@[a-z]+\.[a-z]{2,3}$/gm.test(mail) && !/^\+[0-9]+ \([0-9]+\)+ [0-9]+\-[0-9]+\-[0-9]+$/gm.test(mail)) throw new Error(`Некорректный ввод логина!`)
-    if (!/^[A-Za-z0-9!@#$%^&*()_+-=]{8,}$/gm.test(pwd)) throw new Error(`Некорректный ввод пароля!`)
-    if (pwd !== conPwd) throw new Error (`Пароли не совпадают`)
+    isValid(mail, pwd, conPwd) {
+        if (!/^[a-z0-9_\.-]+@[a-z]+\.[a-z]{2,3}$/gm.test(mail) && !/^\+[0-9]+ \([0-9]+\)+ [0-9]+\-[0-9]+\-[0-9]+$/gm.test(mail)) throw new Error(`Некорректный ввод логина!`)
+        if (!/^[A-Za-z0-9!@#$%^&*()_+-=]{8,}$/gm.test(pwd)) throw new Error(`Некорректный ввод пароля!`)
+        if (pwd !== conPwd) throw new Error(`Пароли не совпадают`)       
+    }
+
+    loginSign() {
+        const btn = document.querySelector(`.sign-btn`);
+        const inp1 = document.querySelector(`.input-1`);
+        const inp2 = document.querySelector(`.input-2`);
+        const inp3 = document.querySelector(`.input-3`);
+        btn.addEventListener(`click`, function () {
+            try {
+                let mail = inp1.value;
+                let pwd = inp2.value;
+                let conPwd = inp3.value;
+
+                this.isValid(mail, pwd, conPwd);
+
+                alert(`Вы успешно зарегистрированы`);
+                inp2.value = ``;
+
+            } catch (error) {
+                alert(error.message);
+            }
+        })
+    }
 }
 
-btn.addEventListener(`click`, function () {
-    try {
-        let mail = inp1.value;
-        let pwd = inp2.value;
-        let conPwd = inp3.value;
-
-        isValid(mail, pwd, conPwd)
-
-        alert(`Вы успешно зарегистрированы`);
-        inp2.value = ``;
-
-    } catch (error) {
-        alert(error.message);
-    }
-})
-
-
+const test = new Test();
+test.loginSign();
